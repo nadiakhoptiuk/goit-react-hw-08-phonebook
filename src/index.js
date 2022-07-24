68bfc7581816d8db6016f0fa92619ab6d955cc49
Nadia Khoptiuk <holineinadia@gmail.com>
2022-07-24 14:34:11 +0300

    add path to browser router

--- src/index.js
+++ src/index.js
@@ -9,15 +9,13 @@ import App from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
-
-// basename = '/goit-react-hw-08-phonebook/';
